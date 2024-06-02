61f27a16ca8fa4d4fdfcad6bbc6de5f527e1d2f7
waseem-wassu <waseemwassu8873@gmail.com>
2024-06-02 17:35:18 +0530

    Initilaizing the app

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const app = express()
 
 const PORT = process.env.PORT 
 
+app.get("/",async(req,res) => {
+    res.status(200).json({status:1,message:"Welcome to our Ecommerce community"})
+})
+
 app.listen(PORT,()=> {
     console.log(`The server is running on ${PORT}`);
 })
